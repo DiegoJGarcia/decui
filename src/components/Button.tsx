c9b977a51abd9443498f55a56f273fb518d7cb3c
Diego <Diegogarciabelver@gmail.com>
2022-07-27 18:20:45 -0300

    aaaHH

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -78,32 +78,3 @@ const Button: FC<ButtonProps> = ({
 };
 
 export default Button;
-
-// import React, { FC, ReactElement } from 'react';
-// import './Button.scss';
-
-// export type TButtonProps = {
-// 	onClick?: () => void;
-// 	disabled?: boolean;
-// 	className?: string;
-// 	children?: any;
-// };
-
-// const Button: FC<TButtonProps> = ({ onClick, disabled, className, children }): ReactElement => {
-// 	return (
-// 		<button
-// 			className={`button${disabled ? ' button--disabled' : ''} labeled${
-// 				className ? ` ${className}` : ''
-// 			}`}
-// 			onClick={e => {
-// 				e?.stopPropagation();
-// 				onClick && onClick();
-// 			}}
-// 			disabled={disabled}
-// 		>
-// 			{children || 'Botón'}
-// 		</button>
-// 	);
-// };
-
-// export default Button;
